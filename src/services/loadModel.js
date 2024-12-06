83916604d793b74668c2271144117247f77f60ba
wishba <wisnubayu34@gmail.com>
2024-12-07 00:43:16 +0700

    load model from cloud storage

--- src/services/loadModel.js
+++ src/services/loadModel.js
@@ -1,8 +1,8 @@
 const tf = require('@tensorflow/tfjs-node');
 
 async function loadModel() {
-  return tf.loadGraphModel('file://./model/model.json');
-  // return tf.loadGraphModel(process.env.MODEL_URL);
+  // return tf.loadGraphModel('file://./model/model.json');
+  return tf.loadGraphModel(process.env.MODEL_URL);
 }
 
 module.exports = loadModel;
