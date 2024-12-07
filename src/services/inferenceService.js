4a5a9ec0365c92ee176d7c0f197bcc5cf50fc7f7
wishba <wisnubayu34@gmail.com>
2024-12-07 11:21:23 +0700

    edit response

--- src/services/inferenceService.js
+++ src/services/inferenceService.js
@@ -9,34 +9,22 @@ async function predictClassification(model, image) {
       .expandDims()
       .toFloat()
 
-    const classes = ['Melanocytic nevus', 'Squamous cell carcinoma', 'Vascular lesion'];
-
     const prediction = model.predict(tensor);
     const score = await prediction.data();
     const confidenceScore = Math.max(...score) * 100;
 
-    const classResult = tf.argMax(prediction, 1).dataSync()[0];
-    const label = classes[classResult];
+    let result, suggestion;
 
-    let explanation, suggestion;
-
-    if (label === 'Melanocytic nevus') {
-      explanation = "Melanocytic nevus adalah kondisi di mana permukaan kulit memiliki bercak warna yang berasal dari sel-sel melanosit, yakni pembentukan warna kulit dan rambut."
-      suggestion = "Segera konsultasi dengan dokter terdekat jika ukuran semakin membesar dengan cepat, mudah luka atau berdarah."
+    if (confidenceScore > 50) {
+      result = "Cancer"
+      suggestion = "Segera periksa ke dokter!"
+    } else {
+      result = "Non-cancer"
+      suggestion = "Penyakit kanker tidak terdeteksi."
     }
 
-    if (label === 'Squamous cell carcinoma') {
-      explanation = "Squamous cell carcinoma adalah jenis kanker kulit yang umum dijumpai. Penyakit ini sering tumbuh pada bagian-bagian tubuh yang sering terkena sinar UV."
-      suggestion = "Segera konsultasi dengan dokter terdekat untuk meminimalisasi penyebaran kanker."
-    }
+    return { result, suggestion };
 
-    if (label === 'Vascular lesion') {
-      explanation = "Vascular lesion adalah penyakit yang dikategorikan sebagai kanker atau tumor di mana penyakit ini sering muncul pada bagian kepala dan leher."
-      suggestion = "Segera konsultasi dengan dokter terdekat untuk mengetahui detail terkait tingkat bahaya penyakit."
-
-    }
-
-    return { confidenceScore, label, explanation, suggestion };
   } catch (error) {
     throw new InputError(`Terjadi kesalahan input: ${error.message}`)
   }
